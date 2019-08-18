7d23141bf938da2aec4eb26d05cafe4e3f4d27d9
khozanathi <50197736+khozanathi@users.noreply.github.com>
2019-08-18 16:36:02 +0200

    bigoikb

--- CC_APIS/function.php
+++ CC_APIS/function.php
@@ -230,14 +230,14 @@ if ($conn->connect_error) {
     
     }
 
-    function package_selection($clientId,$code,$package){
+    function selectPackage($clientId,$code,$package){
         global $conn;
      $sql = "SELECT client.EMAIL,code.CODE_NAME,package.PACKAGE_NAME FROM client,code,package,package_selection 
         WHERE client.CLIENT_ID = package_selection.CLIENT_ID AND
         code.CODE_ID = package_selection.CODE_ID AND package.PACKAGE_ID = package_selection.PACKAGE_ID";   
         $results = $conn->query($sql);
         if($results->num_rows > 0){
-            echo "User Already registered";
+            echo "Package Already selected";
         }
         else
         {
@@ -245,7 +245,7 @@ if ($conn->connect_error) {
         $sql = "INSERT  INTO package_selection (CLIENT_ID,CODE_ID,PACKAGE_ID)
         VALUES ($clientId,$code,$package)";
             if ($conn->query($sql)) {
-                echo "Registered successfully";
+                echo "Package Selected successfully";
                 }
                 else
                 {

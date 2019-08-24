1a89fd427b453c71be6f23b2dd833d9e9c42d0d2
TAbs97 <47631404+TAbs97@users.noreply.github.com>
2019-08-24 20:56:21 +0200

    email validation for login

--- www/js/main.js
+++ www/js/main.js
@@ -38,24 +38,26 @@ function isEmpty(a, b) {
     if (a == null || a == "" || a == '' || a == ' ' || a == undefined || a == " ") {
 
         found = true;
+
     }
 
     if (found) {
         $(b).css({
             "border": "3px solid red",
             "transition": "500ms"
+            
         });
-        
+    console.log(1);
         return found;
     } else {
         $(b).css({
             "border": "3px solid green",
             "transition": "500ms"
         });
-
+console.log(2);
         setTimeout(function () {
             $(b).css({
-                "border": "1px solid #ced4da;",
+                "border": "3px solid #ced4da;",
                 "transition": "500ms"
             });
         }, 2000);
@@ -77,30 +79,35 @@ function isAlphaNumeric(a) {
 }
 
 function isEmail(a, b) {
-    var arr = ("@^.").split("^");
-    var count = 0;
+    // var arr = ("@^.").split("^");
+    var arr = ["@","."];
+    var countAt = 0;
+    var countDot = 0;
     var found = false;
 
+    console.log(arr);
 
     for (var i = 0; i < a.length; i++) {
-        for (var j = 0; j < arr.length; j++) {
-            if (a.charAt(i) == arr[j]) {
-                count++;
-            }
+        if (a.charAt(i) == arr[0]) { //@
+            countAt++;
+        }
+        else if (a.charAt(i) == arr[1]) { //.
+            countDot++;
         }
     }
 
     if (
-        count == 2 &&                       //check if both charactors are in the string (@ .)
+        (countAt == 1 )&&                       //check if both charactors are in the string (@ .)
+        (countDot == 1 )&&                       //check if both charactors are in the string (@ .)
         !(a.indexOf("@.") > -1) &&          //check if both charactors are in this (@.) order
         !(a.indexOf(".@") > -1) &&          //check if both charactors are in this (.@) order
-        !(a.indexOf(".") > 0) &&            //check if fullstop is in the beninging 
-        !(a.indexOf("@") > 0 &&             //check if @ sign is in the beninging
-        !(a.indexOf(".") == a.length)) &&   //check if fullstop is in the end 
-        !(a.indexOf("@") == a.length)       //check if @ sign is in the end
+        !(a.indexOf(".") == 0) &&            //check if fullstop is in the beninging 
+        !(a.indexOf("@") == 0) &&             //check if @ sign is in the beninging
+        !(a.indexOf(".") == a.length-1) &&   //check if fullstop is in the end 
+        !(a.indexOf("@") == a.length-1)       //check if @ sign is in the end
         ) {
-            
         found = true;
+    }else{
     }
 
 
@@ -110,7 +117,7 @@ function isEmail(a, b) {
             "border": "3px solid green",
             "transition": "500ms"
         });
-
+console.log(3);
         setTimeout(function () {
             $(b).css({
                 "border": "1px solid #ced4da;",
@@ -123,6 +130,7 @@ return found;
             "border": "3px solid red",
             "transition": "500ms"
         });
+        console.log(4);
         return found;
     }
 }

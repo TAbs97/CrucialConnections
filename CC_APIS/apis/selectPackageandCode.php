16a1d6fd331265d645541eb8665f4dc2e58554f1
khozanathi <50197736+khozanathi@users.noreply.github.com>
2019-09-11 00:35:50 +0200

    CHANGES

--- CC_APIS/apis/selectPackageandCode.php
+++ CC_APIS/apis/selectPackageandCode.php
@@ -9,13 +9,12 @@ $data = json_decode(file_get_contents('php://input'));
 
 $a = $data->EMAIL;
 $b = $data->CODE_NAME;
-$c = $data->$packageID1;
-$d = $data->$packageID2;
-$e = $data->$packageID3;
+$c = $data->$PACKAGE_NAME;
 
 
 
- $func->selectPackage($a,$b,$c,$d,$e);
+
+ $func->selectPackage($a,$b,$c);
 
 
 
